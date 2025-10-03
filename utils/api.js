883065807578c974c6c088fc04d2f30075a6a5c9
AlexyCatelle <alexy.catelle@oclock.school>
2025-10-03 08:24:31 +0000

    update

--- utils/api.js
+++ utils/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import config from "react-native-config";
 
 const api = axios.create({
-  baseURL: config.API_BASE_URL",
+  baseURL: "http://192.168.1.100:8080/api",
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
